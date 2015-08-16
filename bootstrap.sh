af4d011fee08396dfc1c07ee5944baeec80b3d83
sokolowskik <kamil.j.sokolowski@gmail.com>
2015-08-16 23:50:53 +0200

    personalize the env using my dotfiles repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,9 @@
 sudo apt-get -y install ruby ruby-dev make nodejs git
 sudo gem install jekyll --no-rdoc --no-ri
 
+# dotfiles
+mkdir .dotfiles
+cd .dotfiles
+git clone https://github.com/sokolowskik/dotfiles.git .
+chmod +x bootstrap.sh
+./bootstrap.sh
