5bb820cabefa3950685ace748e6b0f1723503887
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-09-24 13:15:10 +0200

    fix RVM installer signature validation issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 sudo apt-get -y install nodejs git
 
 # rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /home/vagrant/.rvm/scripts/rvm
 
