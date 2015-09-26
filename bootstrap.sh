347530a294e216d9406eaf6130a06a4b8e3843f5
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-09-26 21:43:52 +0200

    resolve RVM dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install nodejs git
+sudo apt-get -y install \
+    nodejs \
+    git \
+    subversion
 
-# rvm
+# RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /home/vagrant/.rvm/scripts/rvm
