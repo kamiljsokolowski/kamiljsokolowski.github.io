4f4572c3736f54747dd7faf5bb8fc4eee6a4d27f
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-09-27 00:46:53 +0200

    use gh-pages-specific interpreter version

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,9 @@ sudo apt-get -y install \
 
 # RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl -sSL https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
+rvm install 2.1.1
 
 # Bundler
 rvm @global do gem install bundler
@@ -18,6 +19,7 @@ rvm gemset create blog-stable
 rvm gemset use  blog-stable
 cd /vagrant
 bundle install
+cd
 
 # dotfiles
 mkdir .dotfiles
