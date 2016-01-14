56d90cdce8ee7893f18a0036b20905b0d311f281
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-01-14 23:04:00 +0100

    automate site builds

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,15 @@
-from fabric.api import env, local
+from fabric.api import env, local, run, cd
+
+code_dir = '/vagrant'
 
 def vagrant():
     env.user = 'vagrant'
     env.hosts = ['127.0.0.1:2222']
     result = local('vagrant ssh-config |grep IdentityFile', capture=True)
     env.key_filename = result.split()[1]
+
+def build(release="stable"):
+    run('rvm gemset use blog-%s' % release)
+    with cd(code_dir):
+        run('bundle install')
 
