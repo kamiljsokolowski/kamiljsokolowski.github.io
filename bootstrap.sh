34e3eea745f74c6d7b826b713d9b5651cfb8f6f9
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-02-03 10:56:20 +0100

    mark blog-stable gemset as the default for ruby 2.1.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,7 @@
 
 sudo apt-get update && sudo apt-get install -y \
     nodejs \
-    git \
-    subversion
+    git
 
 # RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
@@ -16,8 +15,12 @@ rvm @global do gem install bundler
 
 # Jekyll
 rvm gemset create blog-stable
-rvm gemset use  blog-stable
+rvm use 2.1.1@blog-stable --default
+rvm gemset use blog-stable
 cd /vagrant
-bundle install
+if [ -f /vagrant/Gemfile.lock ]; then
+    rm -f /vagrant/Gemfile.lock
+fi
+rvm @blog-stable do bundle install
 cd
 
