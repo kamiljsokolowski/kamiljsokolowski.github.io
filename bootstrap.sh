d54251c76ceaefcad35f6b66e728c5050cf4912b
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-02-07 09:35:38 +0100

    (jekyll-3.0) move old setup to OLDSTABLE and introduce new STABLE

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
 
+OLDSTABLE='2.1.1'
+STABLE='2.1.7'
+
 sudo apt-get update && sudo apt-get install -y \
     nodejs \
     git
 
-# RVM
+# setup Jekyll platform with rvm and bundler
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
-rvm install 2.1.1
 
-# Bundler
-rvm @global do gem install bundler
+for INT in ${OLDSTABLE} ${STABLE}; do
+    rvm install ${INT}
+    rvm ${INT}@global do gem install bundler
+done
 
-# Jekyll
+# (old-stable)
+rvm use ${OLDSTABLE}
+rvm gemset create blog-oldstable
+rvm use ${OLDSTABLE}@blog-oldstable --default
+# need to co version repo tagged github-pages=43 and add dependencies to oldstable gemset
+# ...
+
+# (stable)
+rvm --default use ${STABLE}
 rvm gemset create blog-stable
-rvm use 2.1.1@blog-stable --default
-rvm gemset use blog-stable
+rvm use ${STABLE}@blog-stable --default
 cd /vagrant
 if [ -f /vagrant/Gemfile.lock ]; then
     rm -f /vagrant/Gemfile.lock
