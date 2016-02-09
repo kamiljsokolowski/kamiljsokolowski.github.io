db6d0c27c291f6d7f00ba3bf0378c10baedabfd1
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-02-09 22:37:40 +0100

    implement a set of minor improvments

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-OLDSTABLE='2.1.1'
-STABLE='2.1.7'
+oldstable='2.1.1'
+stable='2.1.7'
 
 sudo apt-get update && sudo apt-get install -y \
     nodejs \
@@ -12,26 +12,26 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
 
-for INT in ${OLDSTABLE} ${STABLE}; do
+for INT in ${oldstable} ${stable}; do
     rvm install ${INT}
     rvm ${INT}@global do gem install bundler
 done
 
 # (old-stable)
-rvm use ${OLDSTABLE}
+rvm use ${oldstable}
 rvm gemset create blog-oldstable
-rvm use ${OLDSTABLE}@blog-oldstable --default
+rvm use ${oldstable}@blog-oldstable --default
 # need to co version repo tagged github-pages=43 and add dependencies to oldstable gemset
 # ...
 
 # (stable)
-rvm --default use ${STABLE}
+rvm --default use ${stable}
 rvm gemset create blog-stable
-rvm use ${STABLE}@blog-stable --default
-cd /vagrant
+rvm use ${stable}@blog-stable --default
+cd /vagrant || exit
 if [ -f /vagrant/Gemfile.lock ]; then
     rm -f /vagrant/Gemfile.lock
 fi
 rvm @blog-stable do bundle install
-cd
+cd || exit
 
